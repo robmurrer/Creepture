5925e60e1f0ae962da76b1d99437dab3ed011347
Rob Murrer <c@robmurrer.com>
2014-04-04 13:59:46 -0400

    Basic GA loop implemented and it appears to work

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,50 @@
 #include <stdio.h>
 #include "test.h"
 
+#define CHROMO_SIZE         2
+#define POP_SIZE            100
+#define MAX_GEN             300
+#define ELITES              0.1
+#define XOVER_RATE          0.5
+#define MUT_RATE            0.05
+#define MUT_RANGE           0.10
+//#define SEED                0
+#define SEED                time(0) 
+
 int main()
 {
-    printf("halo\n");
-    run_tests();
+    //run_tests();
+    srand(SEED);
+    Population pop(POP_SIZE, CHROMO_SIZE);
+
+    int num_elites = (int) (POP_SIZE * ELITES);
+
+    for (int i=0; i<MAX_GEN; i++)
+    {
+        pop.calcFitness();
+        pop.sort();
+
+        // help out weaker members of population
+        for (int j=0; j<num_elites; j++)
+            pop.list[POP_SIZE - (j+1)].crossover(1.0, pop.list[j]);
+
+        // crossover and mutate rest of population
+        for (int j=num_elites; j<POP_SIZE; j++)
+        {
+            pop.list[j].crossover(XOVER_RATE, pop.list[rand()%POP_SIZE]);
+            pop.list[j].mutate(MUT_RANGE, MUT_RANGE);
+        }
+    }
+    
+    pop.calcFitness();
+    pop.sort();
+    pop.print(stdout);
+
+    FILE *out = fopen("../log/chromosomes/test.txt", "w");
+    pop.list[0].print(out);
+    fclose(out);
+
+
 
     return 0;
 }
